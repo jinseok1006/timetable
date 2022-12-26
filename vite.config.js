9bcaf94f0feba58007def216de6ac4343df206b7
jinseok1006 <jinseok1006@naver.com>
2022-12-26 20:11:30 +0900

    template

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: true, // 망내 외부 접속
+    port: 3000,
+  },
+});
